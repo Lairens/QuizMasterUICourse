cbf0e59057accae8240a18326c107356219f7f42
Maxence Mognat <m.mognat@gmail.com>
2022-02-02 11:35:10 -1000

    Gameplay - UI. +Creating logic for the button to change colour when correct answer button is clicked     +for the title to update to "correct" when correct answer button is clicked     +for the title to update to the right answer when a wrong answer button is clicked     +for the correct answer button to update when a wrond answer button is clicked

--- Assets/Scripts/QuizHandler.cs
+++ Assets/Scripts/QuizHandler.cs
@@ -1,15 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class QuizHandler : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO questionSO;
+    [SerializeField] GameObject[] answerButtons;
+    
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
 
     void Start()
     {
         questionText.text = questionSO.GetQuestion;
+
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = questionSO.GetAnswer(i);
+        }
+    }
+
+    public void OnAswerSelected(int index)
+    {
+        if(index == questionSO.GetCorrectAnswerIndex)
+        {
+            questionText.text = "Correct!";
+            Image buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            int correctAnswerIndex = questionSO.GetCorrectAnswerIndex;
+            questionText.text = "Sorry the correct answer was: \n" + questionSO.GetAnswer(correctAnswerIndex);
+            Image correctAnswerButtonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            correctAnswerButtonImage.sprite = correctAnswerSprite;
+        }
     }
 }
